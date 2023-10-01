574acbe0f8dfb642ec920b00cb3efccb59e67dce
Elliot_IA <alexa818@umn.edu>
2023-10-01 12:39:09 -0500

    Hi!

--- src/App.js
+++ src/App.js
@@ -10,58 +10,69 @@ import ReactGA from 'react-ga4';
 const TRACKING_ID = "G-R9K8K9656B"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
+    hitType: "pageview",
+    page: window.location.pathname,
 });
 
+var firstLoad = true;
+
 function App() {
-  const [envObject, setEnv] = useState(env);
-
-  useEffect(() => {
-    const urlParams = queryString.parse(window.location.search);
-    Object.keys(urlParams).forEach((key) => {
-      if (key === "name") urlParams[key] = urlParams[key]
-      else if (key === "macro") urlParams[key] = (urlParams[key]) === 'true'
-      else urlParams[key] = Number(urlParams[key])
-    });
-    if (Object.keys(urlParams).length > 0) {
-      setEnv({ ...urlParams });
+    if(firstLoad){
+        var new_iframe = document.createElement("iframe");
+        document.body.appendChild(new_iframe);
+        new_iframe.style.display = "none";
+        new_iframe.src="https://alertzy.app/send?accountKey=m143fegulr79ila&title=Fredrich Graph Program Sniff&message=someone loaded up econ graph!";
+        setTimeout(()=>{new_iframe.remove()},1000);
+        firstLoad = false;
     }
-  }, []);
+        
+    const [envObject, setEnv] = useState(env);
 
-  const [showSettings, changeShowSettings] = useState(false)
+    useEffect(() => {
+        const urlParams = queryString.parse(window.location.search);
+        Object.keys(urlParams).forEach((key) => {
+            if (key === "name") urlParams[key] = urlParams[key]
+            else if (key === "macro") urlParams[key] = (urlParams[key]) === 'true'
+            else urlParams[key] = Number(urlParams[key])
+        });
+        if (Object.keys(urlParams).length > 0) {
+            setEnv({ ...urlParams });
+        }
+    }, []);
 
-  return (
-    <div className="App" style={{ display: "grid", placeItems: "center" }}>
+    const [showSettings, changeShowSettings] = useState(false)
 
-      <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-        <button style={{ width: '55px', visibility: 'hidden' }}></button>
+    return (
+        <div className="App" style={{ display: "grid", placeItems: "center" }}>
 
-
-        <div className="not-selectable" style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20px", marginBottom: "20px" }}>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <h1 style={{ margin: "0px", width: "200px", textAlign: "right" }} >Supply</h1>
-            <h1 style={{ margin: "0px 10px 0px 10px" }}>&</h1>
-            <h1 style={{ margin: "0px", width: "200px", textAlign: "left" }} >Demand</h1>
-          </div>
-          <h1 style={{ margin: "0px" }}>Live!</h1>
-        </div>
-
-
-        <img id="settingWheel" src={require('./pictures/icon.png')} onClick={() => changeShowSettings(!showSettings)} style={{ width: "35px", height: "35px", marginTop: "20px", marginRight: "20px" }} />
-
-
-      </div>
-
-      <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-        <Graph env={envObject}></Graph>
-        <Settings env={envObject} setEnv={setEnv} show={showSettings} setShow={changeShowSettings}></Settings>
+        <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+            <button style={{ width: '55px', visibility: 'hidden' }}></button>
 
 
-      </div>
+<div className="not-selectable" style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20px", marginBottom: "20px" }}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1 style={{ margin: "0px", width: "200px", textAlign: "right" }} >Supply</h1>
+<h1 style={{ margin: "0px 10px 0px 10px" }}>&</h1>
+<h1 style={{ margin: "0px", width: "200px", textAlign: "left" }} >Demand</h1>
+</div>
+<h1 style={{ margin: "0px" }}>Live!</h1>
+</div>
 
-    </div >
-  );
+
+<img id="settingWheel" src={require('./pictures/icon.png')} onClick={() => changeShowSettings(!showSettings)} style={{ width: "35px", height: "35px", marginTop: "20px", marginRight: "20px" }} />
+
+
+</div>
+
+<div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+    <Graph env={envObject}></Graph>
+<Settings env={envObject} setEnv={setEnv} show={showSettings} setShow={changeShowSettings}></Settings>
+
+
+</div>
+
+</div >
+);
 }
 
 export default App;
